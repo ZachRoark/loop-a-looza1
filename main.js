e51d906e03bf94c1e848389d6e7b8c4eab7288d8
Zach Roark <zach.roark@yahoo.com>
2020-04-29 14:43:34 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// 1. Make an array from the one below called fave_demo 
+// that contains only ages from 18 - 49 
+let ages = [12, 23, 89, 16, 34, 19, 2, 7, 45, 50, 26, 102]
+
+// 2. Loop over the following array and
+// 2a. capitalize 'the'
+// 2b. insert a comma after 'teacher'
+// 2c. output "Yoda says, "The greatest teacher, failure is"
+let yoda_quote = ["the", "greatest", "teacher", "failure", "is"]
